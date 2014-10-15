dbe68eeae90f5ff7e4563b2ff53f8ca9cb2d25d1
William Hart <whart222@users.noreply.github.com>
2014-10-15 13:47:21 +0000

    Updates due to rename of Coopr to Pyomo
    git-svn-id: https://software.sandia.gov/svn/pyomo/pyomo.data/trunk@9279 570ccb8d-5833-0410-9ce8-cbedd0da42eb

--- pyomo/__init__.py
+++ pyomo/__init__.py
@@ -5,7 +5,6 @@
 #  This software is distributed under the BSD License.
 #  Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
 #  the U.S. Government retains certain rights in this software.
-#  For more information, see the Coopr README.txt file.
 #  _________________________________________________________________________
 
 # this is a namespace package
