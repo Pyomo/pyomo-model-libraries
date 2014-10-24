c6a3c46d9bfaf5331b3066bec41d743e930bc570
William Hart <whart222@users.noreply.github.com>
2014-10-24 20:48:13 +0000

    Eliminating dependence on implicit rule declaration.
    git-svn-id: https://software.sandia.gov/svn/pyomo/pyomo.data/trunk@9332 570ccb8d-5833-0410-9ce8-cbedd0da42eb

--- pyomo/data/samples/scripts/s2/knapsack.py
+++ pyomo/data/samples/scripts/s2/knapsack.py
@@ -15,14 +15,14 @@ model.x = Var(model.ITEMS, within=PercentFraction)
 
 def value_rule(model):
     return sum(model.v[i]*model.x[i] for i in model.ITEMS)
-model.value = Objective(sense=maximize)
+model.value = Objective(sense=maximize, rule=value_rule)
 
 def weight_rule(model):
     return sum(model.w[i]*model.x[i] for i in model.ITEMS) <= model.limit
-model.weight = Constraint()
+model.weight = Constraint(rule=weight_rule)
 
 # This constraint is not active, to illustrate how zero dual values are
 # handled by the pyomo command.
 def W_rule(model):
     return sum(model.w[i]*model.x[i] for i in model.ITEMS) <= 2*model.limit
-model.W = Constraint()
+model.W = Constraint(rule=W_rule)
