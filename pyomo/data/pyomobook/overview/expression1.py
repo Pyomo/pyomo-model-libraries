f91a55f2697478b04da142689efc1dadd02da2be
William Hart <whart222@users.noreply.github.com>
2014-11-02 15:23:31 +0000

    Adding expression example
    git-svn-id: https://software.sandia.gov/svn/pyomo/pyomo.data/trunk@9402 570ccb8d-5833-0410-9ce8-cbedd0da42eb

---
+++ pyomo/data/pyomobook/overview/expression1.py
@@ -0,0 +1,26 @@
+from pyomo.environ import *
+
+model = AbstractModel()
+
+model.x = Var(initialize=1.0)
+
+def _e(m,i):
+  return m.x*i
+model.e = Expression([1,2,3],initialize=_e)
+
+instance = model.create()
+
+print value(instance.e[1]) # -> 1.0
+print instance.e[1]()      # -> 1.0
+print instance.e[1].value  # -> a pyomo expression object
+
+# Change the underlying expression
+instance.e[1].value = instance.x**2
+# This requires re-preprocessing
+instance.preprocess()
+
+#... solve
+#... load results
+
+# print the value of the expression given the loaded optimal solution
+print value(instance.e[1]) 
