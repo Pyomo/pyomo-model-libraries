1f37a570ac55e4d1c3877dc207bb03dea3a18fbb
William Hart <whart222@users.noreply.github.com>
2015-03-20 10:59:15 +0000

    Adding a directory for the ref-modeling chapter.
    git-svn-id: https://software.sandia.gov/svn/pyomo/pyomo.data/trunk@9905 570ccb8d-5833-0410-9ce8-cbedd0da42eb

---
+++ pyomo/data/pyomobook/ref-modeling/examples.py
@@ -0,0 +1,149 @@
+from pyomo.environ import *
+
+# --------------------------------------------------
+# @indexed1:
+model = AbstractModel()
+model.A = Set()
+model.x = Var()
+model.o = Objective(expr=model.x)
+model.c = Constraint(expr = model.x >= 0)
+def d_rule(model, a):
+    return a*model.x <= 0
+model.d = Constraint(model.A, rule=d_rule)
+# @:indexed1
+data = DataPortal()
+data['A'] = {None: set([1,2,3])}
+instance = model.create(data)
+instance.pprint()
+
+# --------------------------------------------------
+# @active1:
+model = ConcreteModel()
+model.x = Var([1,2])
+model.c = Constraint(expr= model.x[1] >= 0)
+model.d = Constraint(expr= model.x[2] >= 0)
+def e_rule(model, i):
+    return i*model.x[1] + model.x[2] == 1
+model.e = Constraint([1,2], rule=e_rule)
+
+# Deactivate a single constraint
+print(model.d.active)          # True
+model.d.deactivate()
+print(model.d.active)          # False
+
+# Deactivate a collection of constraints
+print(model.e.active)          # True
+print(model.e[1].active)       # True
+model.e.deactivate()
+print(model.e[1].active)       # False
+print(model.e.active)          # False
+
+# Reactivate a collection of constraints
+model.e.activate()
+print(model.e[1].active)       # True
+
+# Deactivate a single constraint in a collection
+model.e[1].deactivate()
+print(model.e.active)          # True
+print(model.e[1].active)       # False
+# @:active1
+
+# --------------------------------------------------
+# @blocks1:
+model = ConcreteModel()
+model.x = Var()
+model.b = Block()
+model.b.x = Var()
+model.b.y = Var()
+# @:blocks1
+model.pprint()
+
+# --------------------------------------------------
+# @nested1:
+model = ConcreteModel()
+model.b = Block()
+model.b.x = Var()
+
+print(model.b.x.cname())                        # 'b'
+print(model.b.x.cname(True))                    # 'x.b'
+print(model.b.x.cname(fully_qualified=True))    # 'x.b'
+# @:nested1
+
+# --------------------------------------------------
+# @special1:
+model = ConcreteModel()
+model.p = Param([1,2,3], initialize={1:1, 3:3})
+model.q = Param([1,2,3], initialize={1:1, 3:3}, default=0)
+
+# Demonstrating the len() function
+print(len(model.p))                 # 2
+print(len(model.q))                 # 3
+
+# Demonstrating the 'in' operator
+print(2 in model.p)                 # False
+print(2 in model.q)                 # True
+
+# Demonstrating iteration over component keys
+print([key for key in model.p])     # [1,3]
+print([key for key in model.q])     # [1,2,3]
+
+# Demonstrating the '[]' operator
+print(model.p[1])                   # 1
+print(model.q[1])                   # 1
+# @:special1
+
+# --------------------------------------------------
+# @indexed2:
+model = ConcreteModel()
+model.p = Param([1,2,3], initialize={1:1, 3:3})
+model.q = Param([1,2,3], initialize={1:1, 3:3}, default=0)
+
+# Demonstrating the keys() function
+print(model.p.keys())               # [1,3]
+print(model.q.keys())               # [1,2,3]
+
+# Demonstrating the items() function
+print(model.p.items())              # [(1,1), (3,3)]
+print(model.q.items())              # [(1,1), (2,0), (3,3)]
+
+# Demonstrating the values() function
+print(model.p.values())             # [1,3]
+print(model.q.values())             # [1,0,3]
+# @:indexed2
+
+# --------------------------------------------------
+# @indexed3:
+model = ConcreteModel()
+model.p = Param([1,2,3], initialize={1:1, 3:3})
+model.q = Param([1,2,3], initialize={1:1, 3:3}, default=0)
+
+# Demonstrating the keys() function
+print(list(model.p.iterkeys()))     # [1,3]
+print(list(model.q.iterkeys()))     # [1,2,3]
+
+# Demonstrating the items() function
+print(list(model.p.iteritems()))    # [(1,1), (3,3)]
+print(list(model.q.iteritems()))    # [(1,1), (2,0), (3,3)]
+
+# Demonstrating the values() function
+print(list(model.p.itervalues()))   # [1,3]
+print(list(model.q.itervalues()))   # [1,0,3]
+# @:indexed3
+
+# --------------------------------------------------
+# @indexed4:
+model = ConcreteModel()
+model.p = Param([1,2])
+model.q = Param([1,2], [3,4])
+
+# The index set of p is [1,2]
+print(list(model.p.index_set()))
+# The index set of q is [(1,3), (1,4), (2,3), (2,4)]
+print(list(model.q.index_set()))
+
+# Demonstrating the dim() function
+print(model.p.dim())        # 1
+print(model.q.dim())        # 2
+# @:indexed4
+
+
