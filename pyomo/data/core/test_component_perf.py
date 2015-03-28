396e0b21fae3d8f37d04ba1e5278cc003db58947
Gabriel Hackebeil <ghackebeil@users.noreply.github.com>
2015-03-28 20:48:07 +0000

    adding test script for component iteration performance
    git-svn-id: https://software.sandia.gov/svn/pyomo/pyomo.data/trunk@9946 570ccb8d-5833-0410-9ce8-cbedd0da42eb

---
+++ pyomo/data/core/test_component_perf.py
@@ -0,0 +1,118 @@
+import gc
+import os
+thisdir = os.path.dirname(os.path.abspath(__file__))
+
+import pyutilib.th as unittest
+
+from pyomo.core import (ConcreteModel,
+                        Block,
+                        Param,
+                        Set,
+                        Var,
+                        Expression,
+                        Constraint,
+                        Objective,
+                        Reals)
+
+class TestComponentPerformanceBase(object):
+
+    @classmethod
+    def _setUpClass(self, ctype, **kwds):
+        self.model = ConcreteModel()
+        self.model.x = Var()
+        self.model.index = Set(initialize=sorted(range(1000000)))
+        self.model.del_component('test_component')
+        self.model.test_component = \
+            ctype(self.model.index, **kwds)
+
+    def test_iteration(self):
+        cnt = 0
+        for cdata in self.model.\
+            all_component_data.itervalues(self.model.test_component.type()):
+            cnt += 1
+        if self.model.test_component.type() in (Set, Var):
+            self.assertEqual(cnt,
+                             len(self.model.test_component) + 1)
+        else:
+            self.assertEqual(cnt,
+                             len(self.model.test_component))
+
+@unittest.category('performance')
+class TestMutableParamPerformance(unittest.TestCase,
+                                  TestComponentPerformanceBase):
+    @classmethod
+    def setUpClass(self):
+        self._setUpClass(Param,
+                         **{'initialize_as_dense':True,
+                            'initialize':1.0,
+                            'mutable':True})
+
+@unittest.category('performance')
+class TestParamPerformance(unittest.TestCase,
+                           TestComponentPerformanceBase):
+    @classmethod
+    def setUpClass(self):
+        self._setUpClass(Param,
+                         **{'initialize_as_dense':True,
+                            'initialize':1.0,
+                            'mutable':False})
+
+@unittest.category('performance')
+class TestVarPerformance(unittest.TestCase,
+                         TestComponentPerformanceBase):
+    @classmethod
+    def setUpClass(self):
+        self._setUpClass(Var, **{'initialize':1.0})
+
+@unittest.category('performance')
+class TestVarMultiDomainPerformance(unittest.TestCase,
+                                    TestComponentPerformanceBase):
+    @classmethod
+    def setUpClass(self):
+        self._setUpClass(Var,
+                         **{'domain': lambda m,i: Reals})
+
+@unittest.category('performance')
+class TestExpressionPerformance(unittest.TestCase,
+                                    TestComponentPerformanceBase):
+    @classmethod
+    def setUpClass(self):
+        self._setUpClass(Expression,
+                         **{'initialize': 1.0})
+
+@unittest.category('performance')
+class TestConstraintPerformance(unittest.TestCase,
+                                    TestComponentPerformanceBase):
+    @classmethod
+    def setUpClass(self):
+        self._setUpClass(Constraint,
+                         **{'rule': lambda m,i: 1 <= m.x <= 2})
+
+@unittest.category('performance')
+class TestObjectivePerformance(unittest.TestCase,
+                               TestComponentPerformanceBase):
+    @classmethod
+    def setUpClass(self):
+        self._setUpClass(Objective,
+                         **{'rule': lambda m,i: m.x})
+
+@unittest.category('performance')
+class TestSetPerformance(unittest.TestCase,
+                         TestComponentPerformanceBase):
+    @classmethod
+    def setUpClass(self):
+        self._setUpClass(Set,
+                         **{'initialize': [1,2,3]})
+
+@unittest.category('performance')
+class TestBlockPerformance(unittest.TestCase,
+                           TestComponentPerformanceBase):
+    @classmethod
+    def setUpClass(self):
+        self._setUpClass(Block)
+
+if __name__ == "__main__":
+    unittest.main()
+
+
+
