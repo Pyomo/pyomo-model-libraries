d9eb473f86c652d0e238ac77e5661a7f4c9b28ab
William Hart <whart222@users.noreply.github.com>
2015-05-30 15:57:15 +0000

    Misc reformatting.
    git-svn-id: https://software.sandia.gov/svn/pyomo/pyomo.data/trunk@10221 570ccb8d-5833-0410-9ce8-cbedd0da42eb

--- pyomo/data/pyomobook/app/coloring_concrete.py
+++ pyomo/data/pyomobook/app/coloring_concrete.py
@@ -1,5 +1,5 @@
 #
-# Graph coloring exampled adapted from
+# Graph coloring example adapted from
 #
 #  Jonathan L. Gross and Jay Yellen,
 #  "Graph Theory and Its Applications, 2nd Edition",
@@ -8,12 +8,14 @@
 vertices = set(['Ar', 'Bo', 'Br', 'Ch', 'Co', 'Ec', 
                 'FG', 'Gu', 'Pa', 'Pe', 'Su', 'Ur', 'Ve'])
 
-edges = set([('FG','Su'), ('FG','Br'), ('Su','Gu'), ('Su','Br'),
-             ('Gu','Ve'), ('Gu','Br'), ('Ve','Co'), ('Ve','Br'),
-             ('Co','Ec'), ('Co','Pe'), ('Co','Br'), ('Ec','Pe'),
-             ('Pe','Ch'), ('Pe','Bo'), ('Pe','Br'), ('Ch','Ar'),
-             ('Ch','Bo'), ('Ar','Ur'), ('Ar','Br'), ('Ar','Pa'),
-             ('Ar','Bo'), ('Ur','Br'), ('Bo','Pa'), ('Bo','Br'),
+edges = set([('FG','Su'), ('FG','Br'), ('Su','Gu'),
+             ('Su','Br'), ('Gu','Ve'), ('Gu','Br'),
+             ('Ve','Co'), ('Ve','Br'), ('Co','Ec'),
+             ('Co','Pe'), ('Co','Br'), ('Ec','Pe'),
+             ('Pe','Ch'), ('Pe','Bo'), ('Pe','Br'),
+             ('Ch','Ar'), ('Ch','Bo'), ('Ar','Ur'),
+             ('Ar','Br'), ('Ar','Pa'), ('Ar','Bo'),
+             ('Ur','Br'), ('Bo','Pa'), ('Bo','Br'),
              ('Pa','Br')])
 
 ncolors = 4
@@ -33,20 +35,23 @@ model.y = Var()
 # Each node is colored with one color
 model.node_coloring = ConstraintList()
 for v in vertices:
-    model.node_coloring.add(sum(model.x[v,c] for c in colors) == 1)
+    model.node_coloring.add(
+                sum(model.x[v,c] for c in colors) == 1)
 
 # Nodes that share an edge cannot be colored the same
 model.edge_coloring = ConstraintList()
 for v,w in edges:
     for c in colors:
-        model.edge_coloring.add(model.x[v,c] + model.x[w,c] <= 1)
+        model.edge_coloring.add(
+                model.x[v,c] + model.x[w,c] <= 1)
 
 # Provide a lower bound on the minimum number of colors
 # that are needed
 model.min_coloring = ConstraintList()
 for v in vertices:
     for c in colors:
-        model.min_coloring.add(model.y >= c * model.x[v,c])
+        model.min_coloring.add(
+                model.y >= c * model.x[v,c])
 
 # Minimize the number of colors that are needed
 model.obj = Objective(expr=model.y)
