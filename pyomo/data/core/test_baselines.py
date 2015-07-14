6d14dfee1a73eeb7ee63fecf74acd16bbd1bc0c3
Gabriel Hackebeil <ghackebeil@users.noreply.github.com>
2015-07-14 12:26:31 +0000

    fixing issue caused by pyutilib.autotest that was leaving the TempfileManager in a modified state causing temporary solver files to end of littering the local repository in all tests following that suite (still not sure why this happens)
    git-svn-id: https://software.sandia.gov/svn/pyomo/pyomo.data/trunk@10508 570ccb8d-5833-0410-9ce8-cbedd0da42eb

--- pyomo/data/core/test_baselines.py
+++ pyomo/data/core/test_baselines.py
@@ -8,10 +8,21 @@ from os.path import abspath, dirname
 currdir = dirname(abspath(__file__))+os.sep
 
 import pyutilib.th as unittest
-import pyutilib.autotest
+import pyutilib.services
 import pyomo.environ
 
+import pyutilib.autotest
 pyutilib.autotest.create_test_suites(filename=currdir+'test_baselines.yml', _globals=globals())
+
+# GAH: The pyutilib.autotest.create_test_suites function
+#      does some strange things to the TempfileManager state
+#      and I have no idea where these configurations get
+#      updated (I grep'ed for TempfileManager inside pyomo.data
+#      and pyutilib.autotest - nothing). Is this a bug?
+def tearDownModule():
+    pyutilib.services.TempfileManager.clear_tempfiles()
+    pyutilib.services.TempfileManager.tempdir = None
+    pyutilib.services.TempfileManager.unique_files()
 
 if __name__ == "__main__":
     unittest.main()
