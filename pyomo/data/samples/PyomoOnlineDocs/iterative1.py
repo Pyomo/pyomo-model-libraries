50abd8a5aaa28962190d13fac62cd4dcb5a93d87
David Woodruff <DLWoodruff@users.noreply.github.com>
2016-01-04 22:13:03 +0000

    changed the output, mainly so it prints the instance rather that the results object
    git-svn-id: https://software.sandia.gov/svn/pyomo/pyomo.data/trunk@10959 570ccb8d-5833-0410-9ce8-cbedd0da42eb

--- pyomo/data/samples/PyomoOnlineDocs/iterative1.py
+++ pyomo/data/samples/PyomoOnlineDocs/iterative1.py
@@ -21,7 +21,7 @@ model.c = ConstraintList()
 # Create a model instance and optimize
 instance = model.create_instance()
 results = opt.solve(instance)
-print(results)
+instance.display()
 
 # Iterate to eliminate the previously found solution
 for i in range(5):
@@ -36,4 +36,5 @@ for i in range(5):
     instance.c.add( expr >= 1 )
 
     results = opt.solve(instance)
-    print(results)
+    print ("\n===== iteration",i)
+    instance.display()
