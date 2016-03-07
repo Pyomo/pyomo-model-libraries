4dc2deed2bd61e5923f9c33e5bd19e0db5fe93b6
Gabriel Hackebeil <ghackebeil@users.noreply.github.com>
2016-03-07 08:46:24 +0000

    cleanup whitespace
    git-svn-id: https://software.sandia.gov/svn/pyomo/pyomo.data/trunk@11192 570ccb8d-5833-0410-9ce8-cbedd0da42eb

--- pyomo/data/cute/corkscrw_cute.py
+++ pyomo/data/cute/corkscrw_cute.py
@@ -1,13 +1,13 @@
-#  _________________________________________________________________________                                                                                \
-#                                                                                                                                                           \
-#  Pyomo: Python Optimization Modeling Objects                                                                                                           \
-#  Copyright (c) 2010 Sandia Corporation.                                                                                                                   \
-#  This software is distributed under the BSD License.                                                                                                      \
-#  Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,                                                                                   \
-#  the U.S. Government retains certain rights in this software.                                                                                             \
-#  For more information, see the Pyomo README.txt file.                                                                                                     \
-#  _________________________________________________________________________                                                                                \
-
+#  _________________________________________________________________________
+#
+#  Pyomo: Python Optimization Modeling Objects
+#  Copyright (c) 2010 Sandia Corporation.
+#  This software is distributed under the BSD License.
+#  Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+#  the U.S. Government retains certain rights in this software.
+#  For more information, see the Pyomo README.txt file.
+#  _________________________________________________________________________
+#
 # Formulated in Pyomo by Carl D. Laird, Daniel P. Word, and Brandon C. Barrera
 # Taken from:
 
