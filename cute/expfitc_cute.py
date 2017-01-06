dd9d04f15cbffebd3f326a48cb37934faeb128b8
Gabriel Hackebeil <gabe.hackebeil@gmail.com>
2017-01-05 22:21:25 -0500

    cleanup whitespace

--- cute/expfitc_cute.py
+++ cute/expfitc_cute.py
@@ -19,7 +19,7 @@
 # its documentation for any purpose and without fee is hereby
 # granted, provided that the above copyright notice appear in all
 # copies and that the copyright notice and this
-# permission notice appear in all supporting documentation.                     
+# permission notice appear in all supporting documentation.
 
 #   Source:
 #   M.J.D. Powell,
