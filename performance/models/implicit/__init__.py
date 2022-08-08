dfcdbb3a28d78d3df6e892a5509b4198c60d88de
Robert Parker <robbybparker@gmail.com>
2022-08-08 18:15:43 -0400

    update docstring

--- performance/models/implicit/__init__.py
+++ performance/models/implicit/__init__.py
@@ -16,6 +16,16 @@ __doc__ = (
     The "setup_implicit" label corresponds to the creation of a model with
     ExternalGreyBoxBlock components containing the ExternalPyomoModel
     objects that encode the implicit functions.
+    The "solve" label corresponds to the construction of a
+    PyomoNLPWithGreyBoxBlocks object and solution via CyIpopt with repeated
+    evaluation of the function and derivative methods of ExternalPyomoModel.
+    These methods involve heavy use of Pyomo components and expressions
+    (via calculate_variable_from_constraint), so including the solve should
+    be valuable for Pyomo performance testing.
+    It may be less valuable, however, if performance is found to be too
+    highly dependent on Ipopt/ASL versions.
+    Creating the ExternalPyomoModel and NLP objects also involve nl file
+    writes, which may be possible to track individually at a later date.
 
     """
 )
